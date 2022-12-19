e4b4ea61a68a842590fe8b3241cba9d76af80cd7
Ruslan Nizamov <nizamoff.rus@gmail.com>
2022-12-19 11:11:11 +0200

    added sign-in component and firebase utils

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -8,12 +8,12 @@ import {
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyBG4b9AE5Tyz5eMsGiBL0GymVRVLmqg7-M",
+  authDomain: "crwn-db-326c0.firebaseapp.com",
+  projectId: "crwn-db-326c0",
+  storageBucket: "crwn-db-326c0.appspot.com",
+  messagingSenderId: "1078246861187",
+  appId: "1:1078246861187:web:19f551936024a8b21deeb6"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
