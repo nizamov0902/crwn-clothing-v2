d3d0ff881a8cbc3f2a164a611d36ee5fdabd9f6b
Ruslan Nizamov <nizamoff.rus@gmail.com>
2022-12-29 14:57:59 +0100

    added court count feature

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,3 +1,7 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
 import './product-card.styles.scss';
 
 import Button from '../button/button.component';
@@ -5,6 +9,11 @@ import Button from '../button/button.component';
 
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductToCard = () => addItemToCart(product)
+
+
     return (
   <div className='product-card-container'>
     <img src={imageUrl} alt={`${name}`}/>
@@ -12,7 +21,7 @@ const ProductCard = ({ product }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
     </div>
-    <Button buttonType='inverted'>Add to card</Button>
+    <Button buttonType='inverted' onClick={addProductToCard}>Add to card</Button>
   </div>
     )
 }
